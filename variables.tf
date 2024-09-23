df93cb2fb14223811ce5135583d3c951f728ac31
software IDEXUD <102606989+siexud@users.noreply.github.com>
2024-09-23 17:44:51 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "moodle_name" {
+  description = "Nombre del contenedor de Moodle"
+  type        = string
+}
+
+variable "db_name" {
+  description = "Nombre de la base de datos"
+  type        = string
+}
+
+variable "db_user" {
+  description = "Usuario de la base de datos"
+  type        = string
+}
+
+variable "db_password" {
+  description = "Contraseña del usuario de la base de datos"
+  type        = string
+}
